005ec30e8039b9ce24164be905dfa37288f75c09
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-07-24 18:56:08 -0400

    Improve threading library

--- lib/ina-threading/src/lib.rs
+++ lib/ina-threading/src/lib.rs
@@ -4,16 +4,15 @@
 //
 // This file is part of 1N4.
 //
-// 1N4 is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
-// General Public License as published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
+// 1N4 is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
 //
-// 1N4 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
-// General Public License for more details.
+// 1N4 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 //
-// You should have received a copy of the GNU Affero General Public License along with 1N4. If not,
-// see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public License along with 1N4. If not, see
+// <https://www.gnu.org/licenses/>.
 
 //! Provides concurrency solutions for 1N4.
 
@@ -23,10 +22,12 @@ use std::thread::{Builder, JoinHandle};
 
 use tokio::sync::mpsc::error::SendError;
 use tokio::sync::mpsc::{Receiver, Sender};
-use tokio::sync::{RwLock, RwLockMappedWriteGuard, RwLockReadGuard, RwLockWriteGuard};
 
+pub use crate::statics::*;
 pub use crate::threads::*;
 
+/// Provides utilities for static threads.
+mod statics;
 /// Provides definitions for custom threads.
 mod threads;
 
@@ -35,7 +36,7 @@ pub type Result<T, S = Infallible> = std::result::Result<T, Error<S>>;
 
 /// An error that may occur when using this library.
 #[derive(Debug, thiserror::Error)]
-pub enum Error<S> {
+pub enum Error<S = Infallible> {
     /// An IO error.
     #[error(transparent)]
     Io(#[from] std::io::Error),
@@ -148,6 +149,7 @@ where
 }
 
 /// A thread that is automatically joined when dropped.
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Join<H, T>
 where
     H: HandleHolder<T>,
@@ -232,200 +234,5 @@ where
         let result = thread.into_handle().join().unwrap();
 
         self.clean_up_result.unwrap_or(drop)(result);
-    }
-}
-
-/// Wraps a thread and allows it to be safely stored within a static.
-pub enum Static<H, T>
-where
-    H: Send + Sync + HandleHolder<T>,
-    T: Send + Sync + 'static,
-{
-    /// Wraps a thread.
-    Wrap(RwLock<Option<H>>),
-    /// Wraps a joining thread.
-    Join(RwLock<Option<Join<H, T>>>),
-}
-
-impl<H, T> Static<H, T>
-where
-    H: Send + Sync + HandleHolder<T>,
-    T: Send + Sync + 'static,
-{
-    /// Creates a new [`Static::Wrap`] variant.
-    pub const fn wrap() -> Self {
-        Self::Wrap(RwLock::const_new(None))
-    }
-
-    /// Creates a new [`Static::Join`] variant.
-    pub const fn join() -> Self {
-        Self::Join(RwLock::const_new(None))
-    }
-
-    /// Returns a reference to the inner thread.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the thread has not been initialized.
-    pub async fn get(&self) -> StaticReadLock<H, T> {
-        #[inline]
-        fn extract<T>(lock: RwLockReadGuard<Option<T>>) -> RwLockReadGuard<T> {
-            #[allow(clippy::expect_used)]
-            RwLockReadGuard::map(lock, |v| v.as_ref().expect("the thread has not been initialized"))
-        }
-
-        match self {
-            Self::Wrap(h) => StaticReadLock::Wrap(extract(h.read().await)),
-            Self::Join(h) => StaticReadLock::Join(extract(h.read().await)),
-        }
-    }
-
-    /// Returns a mutable reference to the inner thread.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the thread has not been initialized.
-    pub async fn get_mut(&mut self) -> StaticWriteLock<H, T> {
-        #[inline]
-        fn extract<T>(lock: RwLockWriteGuard<Option<T>>) -> RwLockMappedWriteGuard<T> {
-            #[allow(clippy::expect_used)]
-            RwLockWriteGuard::map(lock, |v| v.as_mut().expect("the thread has not been initialized"))
-        }
-
-        match self {
-            Self::Wrap(h) => StaticWriteLock::Wrap(extract(h.write().await)),
-            Self::Join(h) => StaticWriteLock::Join(extract(h.write().await)),
-        }
-    }
-
-    /// Returns a reference to the inner thread.
-    ///
-    /// This blocks the current thread.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the thread has not been initialized or this is called in an asynchronous context.
-    pub fn blocking_get(&self) -> StaticReadLock<H, T> {
-        #[inline]
-        fn extract<T>(lock: RwLockReadGuard<Option<T>>) -> RwLockReadGuard<T> {
-            #[allow(clippy::expect_used)]
-            RwLockReadGuard::map(lock, |v| v.as_ref().expect("the thread has not been initialized"))
-        }
-
-        match self {
-            Self::Wrap(h) => StaticReadLock::Wrap(extract(h.blocking_read())),
-            Self::Join(h) => StaticReadLock::Join(extract(h.blocking_read())),
-        }
-    }
-
-    /// Returns a mutable reference to the inner thread.
-    ///
-    /// This blocks the current thread.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the thread has not been initialized or this is called in an asynchronous context.
-    pub fn blocking_get_mut(&mut self) -> StaticWriteLock<H, T> {
-        #[inline]
-        fn extract<T>(lock: RwLockWriteGuard<Option<T>>) -> RwLockMappedWriteGuard<T> {
-            #[allow(clippy::expect_used)]
-            RwLockWriteGuard::map(lock, |v| v.as_mut().expect("the thread has not been initialized"))
-        }
-
-        match self {
-            Self::Wrap(h) => StaticWriteLock::Wrap(extract(h.blocking_write())),
-            Self::Join(h) => StaticWriteLock::Join(extract(h.blocking_write())),
-        }
-    }
-
-    /// Drops the inner thread handle.
-    ///
-    /// This does nothing if the thread was never initialized.
-    pub async fn drop(&self) {
-        match self {
-            Self::Wrap(h) => *h.write().await = None,
-            Self::Join(h) => *h.write().await = None,
-        }
-    }
-
-    /// Drops the inner thread handle.
-    ///
-    /// This does nothing if the thread was never initialized.
-    ///
-    /// # Panics
-    ///
-    /// Panics if this is called in an asynchronous context.
-    pub fn blocking_drop(&self) {
-        match self {
-            Self::Wrap(h) => *h.blocking_write() = None,
-            Self::Join(h) => *h.blocking_write() = None,
-        }
-    }
-}
-
-/// Wraps a lock for a static thread.
-pub enum StaticReadLock<'rl, H, T>
-where
-    H: Send + Sync + HandleHolder<T>,
-    T: Send + Sync + 'static,
-{
-    /// Wraps a thread.
-    Wrap(RwLockReadGuard<'rl, H>),
-    /// Wraps a joining thread.
-    Join(RwLockReadGuard<'rl, Join<H, T>>),
-}
-
-impl<'rl, H, T> Deref for StaticReadLock<'rl, H, T>
-where
-    H: Send + Sync + HandleHolder<T>,
-    T: Send + Sync + 'static,
-{
-    type Target = H;
-
-    fn deref(&self) -> &Self::Target {
-        match self {
-            Self::Wrap(l) => l,
-            Self::Join(l) => l,
-        }
-    }
-}
-
-/// Wraps a lock for a static thread.
-pub enum StaticWriteLock<'wl, H, T>
-where
-    H: Send + Sync + HandleHolder<T>,
-    T: Send + Sync + 'static,
-{
-    /// Wraps a thread.
-    Wrap(RwLockMappedWriteGuard<'wl, H>),
-    /// Wraps a joining thread.
-    Join(RwLockMappedWriteGuard<'wl, Join<H, T>>),
-}
-
-impl<'rl, H, T> Deref for StaticWriteLock<'rl, H, T>
-where
-    H: Send + Sync + HandleHolder<T>,
-    T: Send + Sync + 'static,
-{
-    type Target = H;
-
-    fn deref(&self) -> &Self::Target {
-        match self {
-            Self::Wrap(l) => l,
-            Self::Join(l) => l,
-        }
-    }
-}
-
-impl<'rl, H, T> DerefMut for StaticWriteLock<'rl, H, T>
-where
-    H: Send + Sync + HandleHolder<T>,
-    T: Send + Sync + 'static,
-{
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        match self {
-            Self::Wrap(l) => l,
-            Self::Join(l) => l,
-        }
     }
 }
---
+++ lib/ina-threading/src/statics.rs
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright © 2024 Jaxydog
+//
+// This file is part of 1N4.
+//
+// 1N4 is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// 1N4 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with 1N4. If not, see
+// <https://www.gnu.org/licenses/>.
+
+use std::marker::PhantomData;
+
+use tokio::sync::{RwLock, RwLockMappedWriteGuard, RwLockReadGuard, RwLockWriteGuard};
+
+use crate::{HandleHolder, Join};
+
+/// A static thread handle.
+#[derive(Debug)]
+pub struct Static<H, T>
+where
+    H: Send + Sync + HandleHolder<T>,
+    T: Send + Sync + 'static,
+{
+    /// The inner handle, locked for mutability purposes.
+    inner: RwLock<Option<H>>,
+    /// The type marker for `T`.
+    _marker: PhantomData<fn(H) -> T>,
+}
+
+impl<H, T> Static<H, T>
+where
+    H: Send + Sync + HandleHolder<T>,
+    T: Send + Sync + 'static,
+{
+    /// Creates a new [`Static<H, T>`].
+    #[inline]
+    pub const fn new() -> Self {
+        Self { inner: RwLock::const_new(None), _marker: PhantomData }
+    }
+
+    /// Creates a new [`Static<H, T>`] containing the given handle.
+    #[inline]
+    pub const fn new_with(handle: H) -> Self {
+        Self { inner: RwLock::const_new(Some(handle)), _marker: PhantomData }
+    }
+
+    /// Returns the asynchronous API for this [`Static<H, T>`].
+    #[inline]
+    pub const fn async_api(&self) -> AsyncStaticApi<H> {
+        AsyncStaticApi { inner: &self.inner }
+    }
+
+    /// Returns the synchronous API for this [`Static<H, T>`].
+    #[inline]
+    pub const fn sync_api(&self) -> SyncStaticApi<H> {
+        SyncStaticApi { inner: &self.inner }
+    }
+}
+
+impl<H, T> Default for Static<H, T>
+where
+    H: Send + Sync + HandleHolder<T>,
+    T: Send + Sync + 'static,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// A joining static thread handle.
+#[derive(Debug)]
+pub struct JoinStatic<H, T>
+where
+    H: Send + Sync + HandleHolder<T>,
+    T: Send + Sync + 'static,
+{
+    /// The inner handle, locked for mutability purposes.
+    inner: RwLock<Option<Join<H, T>>>,
+}
+
+impl<H, T> JoinStatic<H, T>
+where
+    H: Send + Sync + HandleHolder<T>,
+    T: Send + Sync + 'static,
+{
+    /// Creates a new [`JoinStatic<H, T>`].
+    #[inline]
+    pub const fn new() -> Self {
+        Self { inner: RwLock::const_new(None) }
+    }
+
+    /// Creates a new [`JoinStatic<H, T>`] containing the given handle.
+    #[inline]
+    pub const fn new_with(handle: Join<H, T>) -> Self {
+        Self { inner: RwLock::const_new(Some(handle)) }
+    }
+
+    /// Returns the asynchronous API for this [`JoinStatic<H, T>`].
+    #[inline]
+    pub const fn async_api(&self) -> AsyncStaticApi<Join<H, T>> {
+        AsyncStaticApi { inner: &self.inner }
+    }
+
+    /// Returns the synchronous API for this [`JoinStatic<H, T>`].
+    #[inline]
+    pub const fn sync_api(&self) -> SyncStaticApi<Join<H, T>> {
+        SyncStaticApi { inner: &self.inner }
+    }
+}
+
+impl<H, T> Default for JoinStatic<H, T>
+where
+    H: Send + Sync + HandleHolder<T>,
+    T: Send + Sync + 'static,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// An asynchronous API for a static thread.
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug)]
+pub struct AsyncStaticApi<'s, T>
+where
+    T: Send + Sync,
+{
+    /// The inner locked handle.
+    inner: &'s RwLock<Option<T>>,
+}
+
+#[allow(clippy::expect_used)]
+impl<'s, T> AsyncStaticApi<'s, T>
+where
+    T: Send + Sync,
+{
+    /// Returns whether the inner thread is initialized.
+    #[inline]
+    pub async fn has(&self) -> bool {
+        self.inner.read().await.is_some()
+    }
+
+    /// Returns a reference to the inner thread.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the thread has not been initialized.
+    #[inline]
+    pub async fn get(&self) -> RwLockReadGuard<T> {
+        RwLockReadGuard::map(self.inner.read().await, |v| v.as_ref().expect("the thread is not initialized"))
+    }
+
+    /// Returns a mutable reference to the inner thread.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the thread has not been initialized.
+    #[inline]
+    pub async fn get_mut(&self) -> RwLockMappedWriteGuard<T> {
+        RwLockWriteGuard::map(self.inner.write().await, |v| v.as_mut().expect("the thread is not initialized"))
+    }
+
+    /// Sets the inner thread handle, dropping the old value.
+    #[inline]
+    pub async fn set(&self, handle: T) {
+        drop(self.inner.write().await.replace(handle));
+    }
+
+    /// Returns the inner thread handle.
+    ///
+    /// This returns [`None`] if the thread was never initialized.
+    #[inline]
+    pub async fn take(&self) -> Option<T> {
+        self.inner.write().await.take()
+    }
+
+    /// Drops the inner thread handle.
+    ///
+    /// This does nothing if the thread was never initialized.
+    #[inline]
+    pub async fn drop(&self) {
+        drop(self.inner.write().await.take());
+    }
+}
+
+/// A synchronous API for a static thread.
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug)]
+pub struct SyncStaticApi<'s, T> {
+    inner: &'s RwLock<Option<T>>,
+}
+
+#[allow(clippy::expect_used)]
+impl<'s, T> SyncStaticApi<'s, T> {
+    /// Returns whether the inner thread is initialized.
+    ///
+    /// This blocks the current thread.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the thread has not been initialized or this is called in an asynchronous context.
+    #[inline]
+    #[must_use]
+    pub fn has(&self) -> bool {
+        self.inner.blocking_read().is_some()
+    }
+
+    /// Returns a reference to the inner thread.
+    ///
+    /// This blocks the current thread.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the thread has not been initialized or this is called in an asynchronous context.
+    #[inline]
+    pub fn get(&self) -> RwLockReadGuard<T> {
+        RwLockReadGuard::map(self.inner.blocking_read(), |v| v.as_ref().expect("the thread is not initialized"))
+    }
+
+    /// Returns a mutable reference to the inner thread.
+    ///
+    /// This blocks the current thread.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the thread has not been initialized or this is called in an asynchronous context.
+    #[inline]
+    #[must_use]
+    pub fn get_mut(&self) -> RwLockMappedWriteGuard<T> {
+        RwLockWriteGuard::map(self.inner.blocking_write(), |v| v.as_mut().expect("the thread is not initialized"))
+    }
+
+    /// Sets the inner thread handle, dropping the old value.
+    ///
+    /// # Panics
+    ///
+    /// Panics if this is called in an asynchronous context.
+    #[inline]
+    pub fn set(&self, handle: T) {
+        drop(self.inner.blocking_write().replace(handle));
+    }
+
+    /// Returns the inner thread handle.
+    ///
+    /// This returns [`None`] if the thread was never initialized.
+    ///
+    /// # Panics
+    ///
+    /// Panics if this is called in an asynchronous context.
+    #[inline]
+    #[must_use]
+    pub fn take(&self) -> Option<T> {
+        self.inner.blocking_write().take()
+    }
+
+    /// Drops the inner thread handle.
+    ///
+    /// This does nothing if the thread was never initialized.
+    ///
+    /// # Panics
+    ///
+    /// Panics if this is called in an asynchronous context.
+    #[inline]
+    pub fn drop(&self) {
+        drop(self.inner.blocking_write().take());
+    }
+}
