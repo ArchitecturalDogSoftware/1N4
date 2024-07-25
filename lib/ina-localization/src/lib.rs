5039029ec56ca3bafb289fc78a8d73789bfccbe6
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-07-25 14:02:46 -0400

    Partial localizer implementation

---
+++ lib/ina-localization/src/lib.rs
@@ -0,0 +1,257 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright © 2024 Jaxydog
+//
+// This file is part of 1N4.
+//
+// 1N4 is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// 1N4 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with 1N4. If not, see
+// <https://www.gnu.org/licenses/>.
+
+//! Provides localization solutions for 1N4.
+
+use std::collections::HashMap;
+use std::convert::Infallible;
+use std::path::Path;
+
+use clap::{Args, ValueEnum};
+use locale::Locale;
+use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc::error::SendError;
+use translation::Translation;
+
+/// Provides definitions for locales.
+mod locale;
+/// Provides definitions for translations.
+mod translation;
+
+/// A result alias with a defaulted error type.
+pub type Result<T, S = Infallible> = std::result::Result<T, Error<S>>;
+
+/// An error that may occur when using this library.
+#[derive(Debug, thiserror::Error)]
+pub enum Error<S = Infallible> {
+    /// An IO error.
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    /// A threading error.
+    #[error(transparent)]
+    Threading(#[from] ina_threading::Error<S>),
+    /// A sending error.
+    #[error(transparent)]
+    Send(#[from] SendError<S>),
+    /// A TOML deserializing error.
+    #[error(transparent)]
+    FromToml(#[from] toml::de::Error),
+    /// An invalid locale was given.
+    #[error("an invalid locale was provided")]
+    InvalidLocale,
+    /// A translation was missing.
+    #[error("an requested translation was missing")]
+    MissingTranslation,
+    /// A locale was missing.
+    #[error("an requested locale was missing")]
+    MissingLocale,
+}
+
+/// The logger's settings.
+#[non_exhaustive]
+#[derive(Clone, Debug, PartialEq, Eq, Args, Serialize, Deserialize)]
+pub struct Settings {
+    /// The localizer's default locale.
+    #[arg(short = 'l', long = "default-locale", default_value = "en-US")]
+    #[serde(rename = "default-locale")]
+    pub default_locale: Locale,
+
+    /// The directory within which to read language files.
+    #[arg(long = "lang-directory", default_value = "./res/lang/")]
+    #[serde(rename = "directory")]
+    pub file_directory: Box<Path>,
+
+    /// The behavior that the localizer will exhibit when it fails to translate a key.
+    #[arg(long = "lang-miss-behavior", default_value = "key")]
+    #[serde(rename = "miss-behavior")]
+    pub miss_behavior: MissBehavior,
+}
+
+/// The behavior to follow when the localizer is unable to translate a key.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, ValueEnum, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum MissBehavior {
+    /// Returns the raw translation key.
+    Key,
+    /// Returns an error.
+    Error,
+}
+
+impl MissBehavior {
+    /// Calls the miss behavior.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the miss behavior specifies that outcome.
+    pub const fn call<'lc: 'ag, 'ag>(&self, category: &'ag str, key: &'ag str) -> Result<Translation<'lc, 'ag>> {
+        match self {
+            Self::Key => Ok(Translation::Missing(category, key)),
+            Self::Error => Err(Error::MissingTranslation),
+        }
+    }
+}
+
+/// A localizer.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Localizer {
+    /// The localizer's settings.
+    settings: Settings,
+    /// The localizer's stored translations.
+    locales: HashMap<Locale, Translations>,
+}
+
+impl Localizer {
+    /// Creates a new [`Localizer`].
+    #[must_use]
+    pub fn new(settings: Settings) -> Self {
+        Self { settings, locales: HashMap::new() }
+    }
+
+    /// Returns the loaded locales of this [`Localizer`].
+    pub fn locales(&self) -> impl Iterator<Item = Locale> + '_ {
+        self.locales.keys().copied()
+    }
+
+    /// Returns whether this [`Localizer`] has loaded the given locale.
+    #[must_use]
+    pub fn has_locale(&self, locale: Locale) -> bool {
+        self.locales.contains_key(&locale)
+    }
+
+    /// Attempts to load the given locale.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if it fails to read the translation file.
+    pub fn load_locale(&mut self, locale: Locale) -> Result<()> {
+        let path = self.settings.file_directory.join(locale.to_string()).with_extension("toml");
+
+        if !std::fs::exists(&path)? {
+            return Err(Error::MissingLocale);
+        }
+
+        let data = std::fs::read_to_string(path)?;
+        let translations = toml::from_str(&data)?;
+
+        self.locales.insert(locale, translations);
+
+        Ok(())
+    }
+
+    /// Attempts to load the source directory of this [`Localizer`], returning the number of locales loaded.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the localizer fails to load a locale.
+    pub fn load_directory(&mut self) -> Result<usize> {
+        let path = &(*self.settings.file_directory);
+
+        if !std::fs::exists(path)? {
+            return Err(Error::MissingLocale);
+        }
+
+        let mut count: usize = 0;
+
+        for result in std::fs::read_dir(path)? {
+            let entry = result?;
+            let metadata = entry.metadata()?;
+
+            if metadata.is_file() {
+                continue;
+            }
+
+            let path = entry.path();
+            let Some(name) = path.file_stem() else { continue };
+
+            if let Ok(locale) = name.to_string_lossy().parse() {
+                self.load_locale(locale)?;
+
+                count += 1;
+            }
+        }
+
+        Ok(count)
+    }
+}
+
+/// The contents of a translation file.
+#[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Translations {
+    /// The locale that this translation map inherits from.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub inherit: Option<Locale>,
+    /// Translations, sorted into categories.
+    #[serde(default, flatten, skip_serializing_if = "HashMap::is_empty")]
+    pub translations: HashMap<Box<str>, HashMap<Box<str>, Box<str>>>,
+}
+
+impl Translations {
+    /// Returns the parent of this [`Translations`] map.
+    #[must_use]
+    pub const fn parent(&self) -> Option<Locale> {
+        self.inherit
+    }
+
+    /// Returns a translation for a key as written within this specific map.
+    ///
+    /// This method does not check parent maps.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the key is not found and the provided mode specifies to return an error.
+    pub fn get_defined<'lc: 'ag, 'ag>(
+        &'lc self,
+        mode: MissBehavior,
+        category: &'ag str,
+        key: &'ag str,
+    ) -> Result<Translation<'lc, 'ag>> {
+        let Some(map) = self.translations.get(category) else {
+            return mode.call(category, key);
+        };
+        let Some(value) = map.get(key) else {
+            return mode.call(category, key);
+        };
+
+        Ok(Translation::Present(value))
+    }
+
+    /// Returns a translation for a key as written within this or a parent map.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the key is not found and the provided mode specifies to return an error.
+    pub fn get_inherited<'lc: 'ag, 'ag>(
+        &'lc self,
+        mode: MissBehavior,
+        locales: &'lc HashMap<Locale, Self>,
+        category: &'ag str,
+        key: &'ag str,
+    ) -> Result<Translation<'lc, 'ag>> {
+        match self.get_defined(mode, category, key) {
+            Ok(Translation::Inherit(..)) => unreachable!(),
+            Ok(Translation::Missing(..)) | Err(Error::MissingTranslation) if self.parent().is_some() => {
+                let Some(parent) = self.parent() else { unreachable!() };
+                let Some(inherited) = locales.get(&parent) else { return mode.call(category, key) };
+
+                match inherited.get_inherited(mode, locales, category, key) {
+                    Ok(Translation::Present(value)) => Ok(Translation::Inherit(parent, value)),
+                    value => value,
+                }
+            }
+            value => value,
+        }
+    }
+}
---
+++ lib/ina-localization/src/locale.rs
@@ -0,0 +1,186 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright © 2024 Jaxydog
+//
+// This file is part of 1N4.
+//
+// 1N4 is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// 1N4 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with 1N4. If not, see
+// <https://www.gnu.org/licenses/>.
+
+use std::fmt::Display;
+use std::str::FromStr;
+
+use clap::builder::{TypedValueParser, ValueParserFactory};
+use clap::{Arg, Command};
+use serde::de::{Unexpected, Visitor};
+use serde::{Deserialize, Serialize};
+
+use crate::{Error, Result};
+
+/// A locale used within the localizer.
+#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
+pub struct Locale([char; 4]);
+
+impl Locale {
+    /// Creates a new [`Locale`].
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the given locale contains a non-ascii-alphabetic character.
+    pub const fn new(language: [char; 2], territory: [char; 2]) -> Result<Self> {
+        let [l1, l2] = language;
+        let [t1, t2] = territory;
+
+        if !l1.is_ascii_alphabetic()
+            || !l2.is_ascii_alphabetic()
+            || !t1.is_ascii_alphabetic()
+            || !t2.is_ascii_alphabetic()
+        {
+            return Err(Error::InvalidLocale);
+        }
+
+        Ok(Self([l1.to_ascii_lowercase(), l2.to_ascii_lowercase(), t1.to_ascii_uppercase(), t2.to_ascii_uppercase()]))
+    }
+
+    /// Returns the language code of this [`Locale`].
+    pub fn language(&self) -> String {
+        let Self([l1, l2, ..]) = self;
+
+        format!("{l1}{l2}")
+    }
+
+    /// Returns the territory code of this [`Locale`].
+    pub fn territory(&self) -> String {
+        let Self([.., t1, t2]) = self;
+
+        format!("{t1}{t2}")
+    }
+}
+
+impl TryFrom<&str> for Locale {
+    type Error = Error;
+
+    #[inline]
+    fn try_from(value: &str) -> Result<Self> {
+        value.parse()
+    }
+}
+
+impl TryFrom<[char; 4]> for Locale {
+    type Error = Error;
+
+    #[inline]
+    fn try_from([l1, l2, t1, t2]: [char; 4]) -> Result<Self> {
+        Self::new([l1, l2], [t1, t2])
+    }
+}
+
+impl Display for Locale {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self([l1, l2, t1, t2]) = self;
+
+        write!(f, "{l1}{l2}-{t1}{t2}")
+    }
+}
+
+impl FromStr for Locale {
+    type Err = Error;
+
+    fn from_str(value: &str) -> std::result::Result<Self, Self::Err> {
+        #[inline]
+        pub fn next_char(c: &mut impl Iterator<Item = char>, p: impl FnOnce(&char) -> bool) -> Result<char> {
+            let Some(character) = c.next() else { return Err(Error::InvalidLocale) };
+
+            p(&character).then_some(character).ok_or(Error::InvalidLocale)
+        }
+
+        let mut chars = value.chars().take(5);
+
+        let language_1 = next_char(&mut chars, char::is_ascii_alphabetic)?;
+        let language_2 = next_char(&mut chars, char::is_ascii_alphabetic)?;
+
+        next_char(&mut chars, |c| c == &'-')?;
+
+        let territory_1 = next_char(&mut chars, char::is_ascii_alphabetic)?;
+        let territory_2 = next_char(&mut chars, char::is_ascii_alphabetic)?;
+
+        Self::new([language_1, language_2], [territory_1, territory_2])
+    }
+}
+
+impl ValueParserFactory for Locale {
+    type Parser = LocaleParser;
+
+    #[inline]
+    fn value_parser() -> Self::Parser {
+        LocaleParser
+    }
+}
+
+impl Serialize for Locale {
+    #[inline]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+impl<'de> Deserialize<'de> for Locale {
+    #[inline]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct LocaleVisitor;
+
+        impl Visitor<'_> for LocaleVisitor {
+            type Value = Locale;
+
+            fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, "a valid locale string")
+            }
+
+            fn visit_str<E>(self, v: &str) -> std::result::Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                v.parse().map_err(|_| E::invalid_value(Unexpected::Str(v), &self))
+            }
+        }
+
+        deserializer.deserialize_str(LocaleVisitor)
+    }
+}
+
+/// Parses a locale from a command-line argument.
+#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct LocaleParser;
+
+impl TypedValueParser for LocaleParser {
+    type Value = Locale;
+
+    fn parse_ref(
+        &self,
+        cmd: &Command,
+        arg: Option<&Arg>,
+        value: &std::ffi::OsStr,
+    ) -> std::result::Result<Self::Value, clap::Error> {
+        let inner = clap::value_parser!(Box<str>);
+        let value = inner.parse_ref(cmd, arg, value)?;
+
+        if let Ok(locale) = value.parse() {
+            return Ok(locale);
+        }
+
+        Err(clap::Error::new(clap::error::ErrorKind::ValueValidation))
+    }
+}
