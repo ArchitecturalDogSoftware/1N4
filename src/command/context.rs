b83c9a4fc78c84312aedc3ff973ab14e16590d40
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-01-17 01:58:28 -0500

    Update dependencies

--- src/command/context.rs
+++ src/command/context.rs
@@ -79,6 +79,7 @@ where
     }
 
     /// Returns the interaction client of this [`Context<T>`].
+    #[expect(clippy::missing_const_for_fn, reason = "false positive; the arc'd api cannot be const-derefed")]
     pub fn client(&self) -> InteractionClient {
         self.api.client.interaction(self.interaction.application_id)
     }
