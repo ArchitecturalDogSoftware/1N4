b841b6bbc2e1e72717749aa88c2c71b5c7ce2e06
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2025-07-22 22:47:18 -0700

    feat: added more buttons to help message
    Added more buttons onto the `/help` response, providing:
    - Build information
    - Link to the source code
    - Privacy policy
    - Security policy
    Retrieving the build information involved an addition to the build
    script.

--- build.rs
+++ build.rs
@@ -21,23 +21,12 @@
 //! [build script]: <https://doc.rust-lang.org/cargo/reference/build-scripts.html>
 
 use std::fs::File;
-use std::io::BufWriter;
+use std::io::{BufWriter, Read, Write};
 
-use camino::Utf8PathBuf;
+use camino::{Utf8Path, Utf8PathBuf};
 use license_page::CrateList;
 
 fn main() -> std::io::Result<()> {
-    self::generate_license_page()
-}
-
-/// Generates a Markdown file that contains the declared licenses and their full texts of 1N4 and
-/// its dependencies.
-///
-/// This includes both direct and transitive dependencies, but only includes `dev-dependencies` in
-/// builds with `debug_assertions` enabled and never includes `build-dependencies`. The Markdown
-/// file is generated in [CommonMark](https://commonmark.org/) Markdown and is located at
-/// `$OUT_DIR/licenses.md`.
-fn generate_license_page() -> std::io::Result<()> {
     // These environment variables are provided by Cargo, so they should always be present. It
     // looks like Cargo is only handling UTF-8 paths anyways, so it's safe to unwrap on that too.
     //
@@ -47,6 +36,95 @@ fn generate_license_page() -> std::io::Result<()> {
     #[expect(clippy::unwrap_used, reason = "Cargo should define this value and with only UTF-8")]
     let out_dir = Utf8PathBuf::from(std::env::var("OUT_DIR").unwrap());
 
+    self::generate_build_information(&root_dir, &out_dir)?;
+    self::generate_license_page(&root_dir, &out_dir)
+}
+
+/// Generates a file (`$OUT_DIR/build_info.rs`) containing various pieces of information about this
+/// build.
+///
+/// This is positioned _before_ [`self::generate_license_page`] so that this runs every time 1N4 is
+/// built.
+///
+/// Specifically, it provides the following constants, in a format like this example output:
+///
+/// ```ignore
+/// /// The current Git commit hash 1N4 was built at.
+/// pub const COMMIT_HASH: &str = "5f78bcf0be41b4042cf5d20f26ae4aa80d84ee72";
+/// /// The current Cargo features 1N4 was built with.
+/// pub const FEATURES: &str = "default,dotenv";
+/// /// The current target triple 1N4 was built for.
+/// pub const TARGET_TRIPLE: &str = "x86_64-unknown-linux-gnu";
+/// /// The current profile (debug or release) 1N4 was built with.
+/// pub const PROFILE: &str = "debug";
+/// ```
+fn generate_build_information(root_dir: &Utf8Path, out_dir: &Utf8Path) -> std::io::Result<()> {
+    let mut out = BufWriter::new(File::create(out_dir.join("build_info.rs"))?);
+
+    let current_commit = get_current_commit(root_dir)?;
+    writeln!(
+        out,
+        r#"/// The current Git commit hash 1N4 was built at.
+        pub const COMMIT_HASH: &str = "{current_commit}";"#
+    )?;
+
+    #[expect(clippy::unwrap_used, reason = "Cargo should define this value and with only UTF-8")]
+    let features = std::env::var("CARGO_CFG_FEATURE").unwrap();
+    writeln!(
+        out,
+        r#"/// The current Cargo features 1N4 was built with.
+        pub const FEATURES: &str = "{features}";"#
+    )?;
+
+    #[expect(clippy::unwrap_used, reason = "Cargo should define this value and with only UTF-8")]
+    let target_triple = std::env::var("TARGET").unwrap();
+    writeln!(
+        out,
+        r#"/// The current target triple 1N4 was built for.
+        pub const TARGET_TRIPLE: &str = "{target_triple}";"#
+    )?;
+
+    #[expect(clippy::unwrap_used, reason = "Cargo should define this value and with only UTF-8")]
+    let profile = std::env::var("PROFILE").unwrap();
+    writeln!(
+        out,
+        r#"/// The current profile (debug or release) 1N4 was built with.
+        pub const PROFILE: &str = "{profile}";"#
+    )?;
+
+    Ok(())
+}
+
+/// Fetches the current commit directly hash from the `.git` directory at `root_dir`.
+fn get_current_commit(root_dir: &Utf8Path) -> std::io::Result<String> {
+    let git_dir = root_dir.join(".git");
+
+    let mut head_ref = String::new();
+    File::open(git_dir.join("HEAD"))?.read_to_string(&mut head_ref)?;
+    // Trim the trailing line ending in the file.
+    let head_ref = head_ref.trim_ascii_end();
+
+    // Assumes that the contents of `.git/HEAD` will always be either
+    // `refs/heads/BRANCH_NAME` or the commit hash.
+    let Some(current_branch_path) = head_ref.strip_prefix("ref: ") else {
+        return Ok(head_ref.to_string());
+    };
+
+    let mut current_commit = String::new();
+    File::open(git_dir.join(current_branch_path))?.read_to_string(&mut current_commit)?;
+
+    // Trim the trailing line ending in the file.
+    Ok(current_commit.trim_ascii_end().to_string())
+}
+
+/// Generates a Markdown file that contains the declared licenses and their full texts of 1N4 and
+/// its dependencies.
+///
+/// This includes both direct and transitive dependencies, but only includes `dev-dependencies` in
+/// builds with `debug_assertions` enabled and never includes `build-dependencies`. The Markdown
+/// file is generated in [CommonMark](https://commonmark.org/) Markdown and is located at
+/// `$OUT_DIR/licenses.md`.
+fn generate_license_page(root_dir: &Utf8Path, out_dir: &Utf8Path) -> std::io::Result<()> {
     println!("cargo::rerun-if-changed={}", root_dir.join("Cargo.lock"));
 
     let mut out = BufWriter::new(File::create(out_dir.join("licenses.md"))?);
--- src/command/definition/help.rs
+++ src/command/definition/help.rs
@@ -50,7 +50,10 @@ crate::define_entry!("help", CommandType::ChatInput, struct {
 }, struct {});
 
 crate::define_components! {
+    build_information => on_build_information_component;
     licenses => on_licenses_component;
+    privacy_policy => on_privacy_policy_component;
+    security_policy => on_security_policy_component;
 }
 
 /// Executes the command.
@@ -59,7 +62,7 @@ crate::define_components! {
 ///
 /// This function will return an error if the command could not be executed.
 async fn on_command<'ap: 'ev, 'ev>(
-    _: &CommandEntry,
+    command_entry: &CommandEntry,
     mut context: Context<'ap, 'ev, &'ev CommandData>,
     _: CommandOptionResolver<'ev>,
 ) -> EventResult {
@@ -103,19 +106,93 @@ async fn on_command<'ap: 'ev, 'ev>(
 
     let embed = EmbedBuilder::new().title(title).author(author).color(color).description(buffer).footer(footer).build();
 
-    let command_name = self::entry().name;
+    let command_name = command_entry.name;
+
+    let build_information_button = ButtonBuilder::new(ButtonStyle::Secondary)
+        .label(localize!(async(try in locale) category::UI, "help-button-build-information").await?.to_string())?
+        .emoji(EmojiReactionType::Unicode { name: "ℹ️".to_string() })?
+        .custom_id(CustomId::new(command_name, "build_information")?)?
+        .build();
+    let source_code_button = ButtonBuilder::new(ButtonStyle::Link)
+        .url(env!("CARGO_PKG_REPOSITORY"))?
+        .label(localize!(async(try in locale) category::UI, "help-button-source-code").await?.to_string())?
+        .emoji(EmojiReactionType::Unicode { name: "🔗".to_string() })?
+        .build();
     let licenses_button = ButtonBuilder::new(ButtonStyle::Secondary)
         .label(localize!(async(try in locale) category::UI, "help-button-licenses").await?.to_string())?
         .emoji(EmojiReactionType::Unicode { name: "📃".to_string() })?
         .custom_id(CustomId::new(command_name, "licenses")?)?
         .build();
-    let buttons = ActionRowBuilder::new().component(licenses_button)?.build();
+    let privacy_policy_button = ButtonBuilder::new(ButtonStyle::Secondary)
+        .label(localize!(async(try in locale) category::UI, "help-button-privacy-policy").await?.to_string())?
+        .emoji(EmojiReactionType::Unicode { name: "🔐".to_string() })?
+        .custom_id(CustomId::new(command_name, "privacy_policy")?)?
+        .build();
+    let security_policy_button = ButtonBuilder::new(ButtonStyle::Secondary)
+        .label(localize!(async(try in locale) category::UI, "help-button-security-policy").await?.to_string())?
+        .emoji(EmojiReactionType::Unicode { name: "📢".to_string() })?
+        .custom_id(CustomId::new(command_name, "security_policy")?)?
+        .build();
+
+    let buttons = ActionRowBuilder::new()
+        .component(build_information_button)?
+        .component(source_code_button)?
+        .component(licenses_button)?
+        .component(privacy_policy_button)?
+        .component(security_policy_button)?
+        .build();
 
     crate::follow_up_response!(context, struct {
         components: &[buttons.into()],
         embeds: &[embed],
     })
     .await?;
+
+    crate::client::event::pass()
+}
+
+/// Executes the build information component, sending an embed listing properties of this build of
+/// 1N4, such as the version and enabled features.
+///
+/// # Errors
+///
+/// This function will return an error if the component could not be executed.
+async fn on_build_information_component<'ap: 'ev, 'ev>(
+    _: &CommandEntry,
+    mut context: Context<'ap, 'ev, &'ev MessageComponentInteractionData>,
+    _: CustomId,
+) -> EventResult {
+    mod info {
+        include!(concat!(env!("OUT_DIR"), "/build_info.rs"));
+    }
+
+    context.defer(Visibility::Ephemeral).await?;
+
+    let mut buffer = String::new();
+    writeln!(buffer, "- `VERSION`: `{}`", env!("CARGO_PKG_VERSION"))?;
+    writeln!(buffer, "- `FEATURES`: `{}`", info::FEATURES)?;
+    writeln!(buffer, "- `COMMIT_HASH`: `{}`", info::COMMIT_HASH)?;
+    writeln!(buffer, "- `TARGET_TRIPLE`: `{}`", info::TARGET_TRIPLE)?;
+    writeln!(buffer, "- `PROFILE`: `{}`", info::PROFILE)?;
+
+    let locale = match context.as_locale() {
+        Ok(locale) => Some(locale),
+        Err(ina_localizing::Error::MissingLocale) => None,
+        Err(error) => return Err(error.into()),
+    };
+
+    let title = localize!(async(try in locale) category::UI, "help-embed-build-information-header").await?.to_string();
+    let color = color::BRANDING.rgb();
+    let author = if let Some(user) = context.api.cache.current_user() {
+        user.as_embed_author()?
+    } else {
+        let user = context.api.client.current_user().await?.model().await?;
+
+        user.as_embed_author()?
+    };
+
+    let embed = EmbedBuilder::new().title(title).author(author).color(color).description(buffer).build();
+    context.embed(embed, Visibility::Ephemeral).await?;
 
     crate::client::event::pass()
 }
@@ -144,6 +221,72 @@ async fn on_licenses_component<'ap: 'ev, 'ev>(
 
     crate::follow_up_response!(context, struct {
         attachments: &[license_file],
+    })
+    .await?;
+
+    crate::client::event::pass()
+}
+
+/// Executes the privacy policy component, sending a copy of `docs/PRIVACY_POLICY.md` to the user.
+///
+/// # Errors
+///
+/// This function will return an error if the component could not be executed.
+async fn on_privacy_policy_component<'ap: 'ev, 'ev>(
+    _: &CommandEntry,
+    mut context: Context<'ap, 'ev, &'ev MessageComponentInteractionData>,
+    _: CustomId,
+) -> EventResult {
+    const PRIVACY_POLICY_FILE_NAME: &str = "PRIVACY_POLICY.md";
+    const PRIVACY_POLICY_FILE_CONTENT: &[u8] =
+        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/docs/PRIVACY_POLICY.md"));
+    // Almost completely arbitrary. Can be anything, so long as it is unique within the same
+    // message.
+    const PRIVACY_POLICY_FILE_ID: u64 = 0;
+
+    context.defer(Visibility::Ephemeral).await?;
+
+    let privacy_policy_file = Attachment::from_bytes(
+        PRIVACY_POLICY_FILE_NAME.to_string(),
+        PRIVACY_POLICY_FILE_CONTENT.to_vec(),
+        PRIVACY_POLICY_FILE_ID,
+    );
+
+    crate::follow_up_response!(context, struct {
+        attachments: &[privacy_policy_file],
+    })
+    .await?;
+
+    crate::client::event::pass()
+}
+
+/// Executes the security policy component, sending a copy of `docs/SECURITY.md` to the user.
+///
+/// # Errors
+///
+/// This function will return an error if the component could not be executed.
+async fn on_security_policy_component<'ap: 'ev, 'ev>(
+    _: &CommandEntry,
+    mut context: Context<'ap, 'ev, &'ev MessageComponentInteractionData>,
+    _: CustomId,
+) -> EventResult {
+    const SECURITY_POLICY_FILE_NAME: &str = "SECURITY.md";
+    const SECURITY_POLICY_FILE_CONTENT: &[u8] =
+        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/docs/SECURITY.md"));
+    // Almost completely arbitrary. Can be anything, so long as it is unique within the same
+    // message.
+    const SECURITY_POLICY_FILE_ID: u64 = 0;
+
+    context.defer(Visibility::Ephemeral).await?;
+
+    let security_policy_file = Attachment::from_bytes(
+        SECURITY_POLICY_FILE_NAME.to_string(),
+        SECURITY_POLICY_FILE_CONTENT.to_vec(),
+        SECURITY_POLICY_FILE_ID,
+    );
+
+    crate::follow_up_response!(context, struct {
+        attachments: &[security_policy_file],
     })
     .await?;
 
