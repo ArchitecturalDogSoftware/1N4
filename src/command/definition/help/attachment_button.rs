8ffee41f9ba81360430382fcbf634112be7d7893
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2025-07-24 19:06:19 -0700

    feat: added custom attachments and abstracted help buttons
    - Added an `attachment_button!` macro to abstract away creating buttons
      for the `/help` command that attach files.
    - Added support for attaching custom versions of the files instead of
      just the embedded version. Specifically, it looks for the same file
      name as is attached in the Discord message in `res/attachments/`.

---
+++ src/command/definition/help/attachment_button.rs
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright © 2024 Jaxydog
+// Copyright © 2025 RemasteredArch
+//
+// This file is part of 1N4.
+//
+// 1N4 is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// 1N4 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with 1N4. If not, see
+// <https://www.gnu.org/licenses/>.
+
+macro_rules! attachment_button {
+    (
+        $button_id:ident,
+        $localization_key:expr,
+        $icon:expr,
+        $embedded_input_dir:expr,
+        $input_file_name:expr,
+        $output_file_name:expr,
+    ) => {
+        ::paste::paste! {
+            pub mod $button_id {
+                #[doc = "Creates a button (ID `" $button_id "`), which responds with an attachment."]
+                pub async fn button(
+                    locale: Option<::ina_localizing::locale::Locale>,
+                    command_name: &'static ::std::primitive::str
+                ) -> ::anyhow::Result<::twilight_model::channel::message::component::Button> {
+                    let button = $crate::utility::types::builder::ButtonBuilder::new(
+                        ::twilight_model::channel::message::component::ButtonStyle::Secondary
+                    )
+                    .label(
+                        ::ina_localizing::localize!(
+                            async(try in locale) $crate::utility::category::UI, $localization_key
+                        ).await?.to_string()
+                    )?
+                    .emoji(::twilight_model::channel::message::EmojiReactionType::Unicode { name: $icon.to_string() })?
+                    .custom_id($crate::utility::types::custom_id::CustomId::new(command_name, stringify!($button_id))?)?
+                    .build();
+
+                    Ok(button)
+                }
+
+                #[doc = "Executes the `" $button_id "` component, sending either a copy of the given file from `res/attachments/` or an embedded copy."]
+                #[doc = "\n# Errors\n"]
+                #[doc = "This function will return an error if the component could not be executed."]
+                pub async fn [<on_ $button_id _component>]<'ap: 'ev, 'ev>(
+                    _: &$crate::command::registry::CommandEntry,
+                    mut context: $crate::command::context::Context<
+                        'ap,
+                        'ev,
+                        &'ev ::twilight_model::application::interaction::message_component::MessageComponentInteractionData
+                    >,
+                    _: $crate::utility::types::custom_id::CustomId,
+                ) -> $crate::client::event::EventResult {
+                    use ::std::io::Read;
+
+                    const OUTPUT_FILE_NAME: &::std::primitive::str = $output_file_name;
+                    const FILE_CONTENT: &[::std::primitive::u8] = include_bytes!(
+                        concat!($embedded_input_dir, "/", $input_file_name)
+                    );
+                    // Almost completely arbitrary. Can be anything, so long as it is unique within the same message.
+                    const FILE_ID: ::std::primitive::u64 = 0;
+
+                    // TO-DO: this is better as a thread settings call.
+                    let resources_dir = ::std::env::current_dir()
+                        .map_or_else(|_| ::std::path::PathBuf::from("./res/attachments"), |v| v.join("res/attachments"));
+
+                    let mut buf = ::std::vec::Vec::new();
+                    let file_content = ::std::fs::File::open(resources_dir.join($output_file_name))
+                        .and_then(|mut f| f.read_to_end(&mut buf).map(|_| buf.as_slice()))
+                        .unwrap_or(FILE_CONTENT);
+
+                    context.defer($crate::command::context::Visibility::Ephemeral).await?;
+
+                    let license_file = ::twilight_model::http::attachment::Attachment::from_bytes(
+                        OUTPUT_FILE_NAME.to_string(),
+                        file_content.to_vec(),
+                        FILE_ID,
+                    );
+
+                    $crate::follow_up_response!(context, struct {
+                        attachments: &[license_file],
+                    })
+                    .await?;
+                    context.complete();
+
+                    $crate::client::event::pass()
+                }
+            }
+        }
+    };
+
+    ($button_id:ident, $localization_key:expr, $icon:expr, $embedded_input_dir:expr, $input_file_name:expr,) => {
+        attachment_button!(
+            $button_id,
+            $localization_key,
+            $icon,
+            $embedded_input_dir,
+            $input_file_name,
+            $input_file_name,
+        );
+    }
+}
+
+attachment_button!(licenses, "help-button-licenses", "📃", env!("OUT_DIR"), "licenses.md",);
+attachment_button!(
+    privacy_policy,
+    "help-button-privacy-policy",
+    "🔐",
+    concat!(env!("CARGO_MANIFEST_DIR"), "/docs"),
+    "PRIVACY_POLICY.md",
+);
+attachment_button!(
+    security_policy,
+    "help-button-security-policy",
+    "📢",
+    concat!(env!("CARGO_MANIFEST_DIR"), "/docs"),
+    "SECURITY.md",
+    "SECURITY_POLICY.md",
+);
