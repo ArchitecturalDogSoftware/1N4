26ae19e7cb70a4e9d3dc56a184a04e62ec5e82f3
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-12-28 10:45:44 -0500

    refactor(storage): add locking to file read/write operations

--- lib/ina-storage/src/system/file.rs
+++ lib/ina-storage/src/system/file.rs
@@ -67,11 +67,43 @@ impl DataReader for FileSystem {
     }
 
     fn blocking_read(&self, path: &Path) -> Result<Arc<[u8]>, Self::Error> {
-        Ok(std::fs::read(path)?.into())
+        let mut file = std::fs::File::open(path)?;
+
+        file.lock_shared()?;
+
+        let file_size = file.metadata().map_or(0, |metadata| {
+            // The vector may be at most `isize::MAX` bytes.
+            usize::try_from(metadata.len()).unwrap_or(isize::MAX as usize)
+        });
+        let mut buffer = Vec::with_capacity(file_size);
+
+        std::io::Read::read_to_end(&mut file, &mut buffer)?;
+
+        file.unlock()?;
+
+        Ok(buffer.into())
     }
 
     async fn read(&self, path: &Path) -> Result<Arc<[u8]>, Self::Error> {
-        Ok(tokio::fs::read(path).await?.into())
+        let file = tokio::fs::File::open(path).await?;
+
+        // Currently, `lock` is not implemented in `tokio` due to the MSRV requirement.
+        // Because of this, we need to juggle between the stdlib and tokio file types.
+        let file = file.into_std().await;
+        file.lock_shared()?;
+        let mut file = tokio::fs::File::from_std(file);
+
+        let file_size = file.metadata().await.map_or(0, |metadata| {
+            // The vector may be at most `isize::MAX` bytes.
+            usize::try_from(metadata.len()).unwrap_or(isize::MAX as usize)
+        });
+        let mut buffer = Vec::with_capacity(file_size);
+
+        tokio::io::AsyncReadExt::read_to_end(&mut file, &mut buffer).await?;
+
+        file.into_std().await.unlock()?;
+
+        Ok(buffer.into())
     }
 }
 
@@ -83,7 +115,22 @@ impl DataWriter for FileSystem {
             std::fs::create_dir_all(path)?;
         }
 
-        std::fs::write(path, bytes)
+        // We have to use `options` here because `File::create` will truncate before the lock is acquired.
+        let mut file = if self.blocking_exists(path)? {
+            std::fs::File::options().write(true).open(path)?
+        } else {
+            std::fs::File::options().create_new(true).write(true).open(path)?
+        };
+
+        file.lock()?;
+
+        // Try to resize to match the length of the byte array, truncating to zero if the value is too large.
+        // Realistically, since 128-bit systems are not commonplace, this is unnecessary and will always succeed.
+        file.set_len(bytes.len().try_into().unwrap_or(0))?;
+
+        std::io::Write::write_all(&mut file, bytes)?;
+
+        file.unlock()
     }
 
     async fn write(&mut self, path: &Path, bytes: &[u8]) -> Result<(), Self::Error> {
@@ -91,12 +138,40 @@ impl DataWriter for FileSystem {
             tokio::fs::create_dir_all(path).await?;
         }
 
-        tokio::fs::write(path, bytes).await
+        // We have to use `options` here because `File::create` will truncate before the lock is acquired.
+        let file = if self.exists(path).await? {
+            tokio::fs::File::options().write(true).open(path).await?
+        } else {
+            tokio::fs::File::options().create_new(true).write(true).open(path).await?
+        };
+
+        // Currently, `lock` is not implemented in `tokio` due to the MSRV requirement.
+        // Because of this, we need to juggle between the stdlib and tokio file types.
+        let file = file.into_std().await;
+        file.lock()?;
+        let mut file = tokio::fs::File::from_std(file);
+
+        // Try to resize to match the length of the byte array, truncating to zero if the value is too large.
+        // Realistically, since 128-bit systems are not commonplace, this is unnecessary and will always succeed.
+        file.set_len(bytes.len().try_into().unwrap_or(0)).await?;
+
+        tokio::io::AsyncWriteExt::write_all(&mut file, bytes).await?;
+
+        file.into_std().await.unlock()
     }
 
     fn blocking_rename(&mut self, from: &Path, into: &Path) -> Result<(), Self::Error> {
         if let Some(path) = into.parent() {
             std::fs::create_dir_all(path)?;
+        }
+
+        if self.blocking_exists(into)? {
+            let file = std::fs::File::open(into)?;
+
+            // Acquire an exclusive lock on the file to ensure nothing else is currently using it.
+            file.lock()?;
+            // Then immediately drop it so that we can safely overwrite the file.
+            file.unlock()?;
         }
 
         std::fs::rename(from, into)
@@ -105,6 +180,17 @@ impl DataWriter for FileSystem {
     async fn rename(&mut self, from: &Path, into: &Path) -> Result<(), Self::Error> {
         if let Some(path) = into.parent() {
             tokio::fs::create_dir_all(path).await?;
+        }
+
+        if self.exists(into).await? {
+            // Currently, `lock` is not implemented in `tokio` due to the MSRV requirement.
+            // Because of this, we need to juggle between the stdlib and tokio file types.
+            let file = tokio::fs::File::open(into).await?.into_std().await;
+
+            // Acquire an exclusive lock on the file to ensure nothing else is currently using it.
+            file.lock()?;
+            // Then immediately drop it so that we can safely overwrite the file.
+            file.unlock()?;
         }
 
         tokio::fs::rename(from, into).await
